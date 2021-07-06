29568c4ab9b7516abc3b5fcaaea8a484a75bdf7a
Prince0047 <sonubhadana838@gmail.com>
2021-07-06 10:27:28 +0530

    Add routes

---
+++ src/components/lists/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Lists = () => {
+  return <div>lists</div>;
+};
+
+export default Lists;
---
+++ src/components/styles/menubar.js
@@ -0,0 +1,52 @@
+import styled from 'styled-components';
+
+export const Header = styled.header`
+  padding-left: 10px;
+  padding-right: 10px;
+  .selected {
+    .active {
+      fill: rgba(29, 161, 242, 1);
+      color: rgba(29, 161, 242, 1);
+      border-radius: 50px;
+    }
+  }
+`;
+
+export const MenuItem = styled.div`
+  ${(props) => props.logo && { display: 'inline-block' }};
+  margin-top: 10px;
+  color: #000000;
+  div {
+    display: inline-block;
+    padding: 10px;
+  }
+  &:hover div {
+    color: rgba(29, 161, 242, 1);
+    fill: rgba(29, 161, 242, 1);
+    background: rgba(29, 161, 242, 0.1);
+    border-radius: 50px;
+  }
+`;
+
+export const MenuTitle = styled.span`
+  margin-left: 20px;
+  font-size: 19px;
+  font-weight: bold;
+  line-height: 1.3;
+  text-transform: capitalize;
+`;
+
+export const Button = styled.button`
+  width: 100%;
+  background: rgba(29, 161, 242, 1);
+  border: none;
+  border-radius: 50px;
+  font-size: 15px;
+  font-weight: bold;
+  color: #ffffff;
+  text-align: center;
+  padding: 12px 30px;
+  &:hover {
+    background: rgb(26, 145, 218);
+  }
+`;
