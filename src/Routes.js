3433444154725783deb6c65836a7cfad582ba66c
Prince0047 <sonubhadana838@gmail.com>
2021-07-08 19:50:30 +0530

    login

--- src/Routes.js
+++ src/Routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { Row, Col } from 'antd';
 import MenuBar from './components/menubar/index';
+import SignIn from './components/signin/index';
 import Home from './components/home/index';
 import Explore from './components/explore/index';
 import Notifications from './components/notifications/index';
@@ -27,7 +28,7 @@ const Routes = () => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path='/' component={withMenuBar(Home)} />
+        <Route exact path='/' component={SignIn} />
         <Route path='/home' component={withMenuBar(Home)} />
         <Route path='/explore' component={withMenuBar(Explore)} />
         <Route path='/notifications' component={withMenuBar(Notifications)} />
--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import { Provider } from 'react-dom';
+import store from './store';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
